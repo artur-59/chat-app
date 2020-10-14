673a8b5c6c1242265bfe6e3cf37d6033a41214c6
artur-59 <arturfernandes59@gmail.com>
2020-10-14 10:21:38 +0100

    added ContactList file and made props changes on Contact file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Contact from './components/Contact';
 import './App.css';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
       <Contact />
+      <ContactList />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,23 +5,21 @@ const avatar = <img src="https://randomuser.me/api/portraits/women/54.jpg" alt="
 
 const name = 'Judy Herrera';
 
-const isAvailable = true;
-
-const status = isAvailable ? "Online" : "Offline";
+const online = {props.online};
 
 
-function Contact() {
+function Contact(props) {
   return (
     <div>
         <figure class="Contact">
-            <img  class="avatar" src="https://randomuser.me/api/portraits/women/54.jpg" alt="Judy Herrera picture" />
+            <img  class="avatar" src={props.avatar} alt={props.name} />
       <figcaption>
         <blockquote class="name">
-          {name}
+          {props.name}
         </blockquote>
-        <div class="status">
-        <div class="status-online"></div>
-        <p class="status-text">{status}</p></div>
+        <div class="status" {props.online ? "Online" : "Offline"}>
+        <div class="status-online" {props.online ? "Online" : "Offline"}></div>
+        <p class="status-text">{props.online}</p></div>
       </figcaption>
     </figure>
     </div>
