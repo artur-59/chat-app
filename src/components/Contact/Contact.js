c9215ed2d9b641e8469f7721457d4b43debca661
artur-59 <arturfernandes59@gmail.com>
2020-10-15 10:56:04 +0100

    changed Contact.js function to a class component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,24 +2,53 @@ import React from 'react';
 import './Contact.css';
 
 
-function Contact(props) {
-  const { name, avatar, online } = props;
-  return (
-    <div>
-        <figure class="Contact">
-            <img  class="avatar" src={avatar} alt={name} />
-      <figcaption>
-        <blockquote class="name">
-          {name}
-        </blockquote>
-        <div class="status">
-        <div class={online ? "status-online" : "status-offline"}></div>
-        <p class="status-text">{online ? "online" : "offline"}</p></div>
-      </figcaption>
-    </figure>
-    </div>
-  );
+class Contact extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {online: props.online};
+  }
+  render(){
+    return(
+      <div>
+         <figure class="Contact">
+             <img  class="avatar" src={avatar} alt={name} />
+       <figcaption>
+         <blockquote class="name">
+           {name}
+         </blockquote>
+         <div class="status">
+         <div class={online ? "status-online" : "status-offline"}
+         onClick={event => {
+                const newStatus = !this.state.online;
+                this.setState({ online: newStatus });
+              }}></div>
+         <p class="status-text">{online ? "online" : "offline"}</p></div>
+       </figcaption>
+     </figure>
+     </div>
+    );
+  }
 }
+
+
+// function Contact(props) {
+//   const { name, avatar, online } = props;
+//   return (
+//     <div>
+//         <figure class="Contact">
+//             <img  class="avatar" src={avatar} alt={name} />
+//       <figcaption>
+//         <blockquote class="name">
+//           {name}
+//         </blockquote>
+//         <div class="status">
+//         <div class={online ? "status-online" : "status-offline"}></div>
+//         <p class="status-text">{online ? "online" : "offline"}</p></div>
+//       </figcaption>
+//     </figure>
+//     </div>
+//   );
+// }
 
 
 export default Contact;
