42c2caf03a313c18d703c4468d5557bd8a6ff4db
artur-59 <arturfernandes59@gmail.com>
2020-10-15 16:16:41 +0100

    converted function to class component properly

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,24 +5,23 @@ import './Contact.css';
 class Contact extends React.Component {
   constructor(props){
     super(props);
-    this.state = {online: props.online};
+    this.state = { online: this.props.online }
   }
+  statusReverse = () => {
+      this.setState({online: !this.state.online})
+    };
   render(){
     return(
       <div>
-         <figure class="Contact">
-             <img  class="avatar" src={avatar} alt={name} />
-       <figcaption>
+        <figure class="Contact">
+            <img  class="avatar" src={this.props.avatar} alt={this.props.name} />
+      <figcaption>
          <blockquote class="name">
-           {name}
+           {this.props.name}
          </blockquote>
-         <div class="status">
-         <div class={online ? "status-online" : "status-offline"}
-         onClick={event => {
-                const newStatus = !this.state.online;
-                this.setState({ online: newStatus });
-              }}></div>
-         <p class="status-text">{online ? "online" : "offline"}</p></div>
+         <div class="status" onClick={this.statusReverse}>
+         <div class={this.state.online ? "status-online" : "status-offline"} onClick={this.statusReverse}></div>
+        <p onClick={this.statusReverse} class="status-text">{this.state.online ? "online" : "offline"}</p></div>
        </figcaption>
      </figure>
      </div>
